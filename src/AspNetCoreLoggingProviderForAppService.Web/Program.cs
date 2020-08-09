0510206a2b18f084fb06245350e4cd71b2ca5dab
Mickaël Derriey <mderriey@gmail.com>
2020-08-09 16:27:52 +1000

    Add App Service logging NuGet package and logging provider

--- src/AspNetCoreLoggingProviderForAppService.Web/Program.cs
+++ src/AspNetCoreLoggingProviderForAppService.Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace AspNetCoreLoggingProviderForAppService.Web
 {
@@ -12,6 +13,7 @@ namespace AspNetCoreLoggingProviderForAppService.Web
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(builder => builder.AddAzureWebAppDiagnostics())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
